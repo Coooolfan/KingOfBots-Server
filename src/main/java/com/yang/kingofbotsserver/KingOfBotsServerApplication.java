d15d6ece8575c214914ecf7ffd34c5157b8064fe
YangYiFan <Coolfan1024@gmail.com>
2023-11-23 19:52:38 +0800

    First commit.

---
+++ src/main/java/com/yang/kingofbotsserver/KingOfBotsServerApplication.java
@@ -0,0 +1,13 @@
+package com.yang.kingofbotsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingOfBotsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingOfBotsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yang/kingofbotsserver/KingOfBotsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.kingofbotsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingOfBotsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
