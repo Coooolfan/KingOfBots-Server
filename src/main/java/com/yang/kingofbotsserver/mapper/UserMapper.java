3986c697b68df79f9959e4b43a6fc67369d85229
YangYiFan <Coolfan1024@gmail.com>
2024-02-24 16:35:41 +0800

    配置Mysql, Spring Boot Starter JDBC, Project Lombok, MySQL Connector/J, mybatis-plus-boot-starter, mybatis-plus-generator, spring-boot-starter-security.

---
+++ src/main/java/com/yang/kingofbotsserver/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.yang.kingofbotsserver.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yang.kingofbotsserver.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ src/main/java/com/yang/kingofbotsserver/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.yang.kingofbotsserver.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yang.kingofbotsserver.mapper.UserMapper;
+import com.yang.kingofbotsserver.pojo.User;
+import com.yang.kingofbotsserver.service.impl.utils.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        User user = userMapper.selectOne(queryWrapper);
+        if(user==null){
+            throw new RuntimeException("用户不存在 com.yang.kingofbotserver.service.impl.UserDetailsServiceImpl");
+        }
+        return new UserDetailsImpl(user);
+    }
+}
