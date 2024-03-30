cd2a0616d65571b9ba37380fac8f9d96ac7e9b72
YangYiFan <Coolfan1024@gmail.com>
2024-03-30 15:28:09 +0800

    后端地图生成

--- src/main/java/com/yang/kingofbotsserver/consumer/WebSocketServer.java
+++ src/main/java/com/yang/kingofbotsserver/consumer/WebSocketServer.java
@@ -1,7 +1,9 @@
 package com.yang.kingofbotsserver.consumer;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.yang.kingofbotsserver.mapper.UserMapper;
 import com.yang.kingofbotsserver.pojo.User;
+import com.yang.kingofbotsserver.utils.GameMapUtil;
 import com.yang.kingofbotsserver.utils.JwtUtil;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
@@ -10,13 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
 @ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
 public class WebSocketServer {
     //    线程安全的hashmap
     private static final ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
     private User user;
     private Session session = null;
     private static UserMapper userMapper;
@@ -28,6 +33,7 @@ public class WebSocketServer {
 
     @OnOpen
     public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+        // 建立连接
         this.session = session;
         int userID = JwtUtil.getUserId(token);
         if (userID == -1) {
@@ -40,8 +46,7 @@ public class WebSocketServer {
             return;
         }
         users.put(userID, this);
-        System.out.println("Connected" + user);
-        // 建立连接
+        System.out.println("Connected " + user);
     }
 
     @OnClose
@@ -50,6 +55,7 @@ public class WebSocketServer {
         // 关闭链接
         if (this.user != null) {
             users.remove(user.getId());
+            matchpool.remove(this.user);
         }
     }
 
@@ -57,6 +63,49 @@ public class WebSocketServer {
     public void onMessage(String message, Session session) {
         System.out.println("New Msg!!!");
         // 从Client接收消息
+        System.out.println("receive message!");
+        JSONObject data = JSONObject.parseObject(message);
+        String event = data.getString("event");
+        if ("start-matching".equals(event)) {
+            startMatching();
+        } else if ("stop-matching".equals(event)) {
+            stopMatching();
+        }
+
+    }
+
+    private void stopMatching() {
+        matchpool.remove(this.user);
+    }
+
+    private void startMatching() {
+        System.out.println("start matching!");
+        matchpool.add(this.user);
+
+        while (matchpool.size() >= 2) {
+            Iterator<User> it = matchpool.iterator();
+            User a = it.next(), b = it.next();
+            matchpool.remove(a);
+            matchpool.remove(b);
+
+            GameMapUtil game = new GameMapUtil(13, 14, 20);
+            game.createMap();
+
+            JSONObject respA = new JSONObject();
+            respA.put("event", "start-matching");
+            respA.put("opponent_username", b.getUsername());
+            respA.put("opponent_photo", b.getPhoto());
+            respA.put("gamemap", game.getG());
+            users.get(a.getId()).sendMessage(respA.toJSONString());
+
+            JSONObject respB = new JSONObject();
+            respB.put("event", "start-matching");
+            respB.put("opponent_username", a.getUsername());
+            respB.put("opponent_photo", a.getPhoto());
+            respB.put("gamemap", game.getG());
+            users.get(b.getId()).sendMessage(respB.toJSONString());
+        }
+
     }
 
     @OnError
